2dbb9ec1a2c4abac2f068ff325373e3b457cc5d3
pschatzmann <phil.schatzmann@gmail.com>
2023-02-03 10:08:52 +0100

    remove build

--- examples/parse/build/CMakeFiles/midi-parse.dir/Users/pschatzmann/Dev/Arduino/libraries/arduino-midi-fileparser/examples/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/midi-parse.dir/Users/pschatzmann/Dev/Arduino/libraries/arduino-midi-fileparser/examples/main.cpp.o: \
-  /Users/pschatzmann/Dev/Arduino/libraries/arduino-midi-fileparser/examples/main.cpp
